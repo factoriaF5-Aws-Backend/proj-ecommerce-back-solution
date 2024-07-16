abe4d19a4d2e5af8a3e8e818463049c5c35a67a0
Amr Hefny <ahefny1987@gmail.com>
2024-07-16 11:20:52 +0200

    Create a Spring boot project

---
+++ src/main/java/com/factoriaf5/ecommerceManager/EcommerceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.factoriaf5.ecommerceManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/factoriaf5/ecommerceManager/EcommerceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factoriaf5.ecommerceManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
